cce39ddfaf768267d533128cf118c125854eb962
Melissa <lynch.melissat@gmail.com>
2024-08-02 17:38:30 -0400

    added delete button and more info

--- src/components/views/AllTasksView.jsx
+++ src/components/views/AllTasksView.jsx
@@ -17,6 +17,8 @@ let liStyleLastChild = {
 import { Link } from "react-router-dom";
 
 function AllTasksView({tasks}) {
+  let deleteTask = (taskId) => {};
+
   if (!tasks.length) {
     return (
       <>
@@ -28,12 +30,19 @@ function AllTasksView({tasks}) {
   return (
     <>
       <Link to={`/`}><button>Back to Home</button></Link>
-      <ul style={ulStyle}>
+      <div style={ulStyle}>
         {tasks.map((todo, idx) => {
           let styleBool = idx === tasks.length - 1 ? liStyleLastChild : liStyle;
-          return <li key={todo.id} style={styleBool}>Task #{idx+1}: {todo.title}</li>;
+          return (
+            <div key={todo.id} style={styleBool}>
+              <h4>Task #{idx+1}: {todo.title}</h4>
+              <h5>Assigned to: Employee {todo.userId}</h5>
+              <h6>{todo.completed ? "COMPLETED" : "IN PROGRESS"}</h6>
+              <button onClick={() => deleteTask(todo.id)}>Delete</button>
+            </div>
+          );
         })}
-      </ul>
+      </div>
     </>
   );
 
