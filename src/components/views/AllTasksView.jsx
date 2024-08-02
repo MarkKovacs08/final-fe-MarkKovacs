23db6c33e34c413b6a9d10a70c5148698ce90bb4
Melissa <lynch.melissat@gmail.com>
2024-08-02 17:16:21 -0400

    added border and padding

--- src/components/views/AllTasksView.jsx
+++ src/components/views/AllTasksView.jsx
@@ -1,19 +1,39 @@
+let ulStyle = {
+  listStyleType: "none",
+  padding: 0,
+  border: "1px solid #d0d",
+};
+
+let liStyle = {
+  padding: "8px 16px",
+  borderBottom: "1px solid #d0d",
+};
+
+let liStyleLastChild = {
+  ...liStyle,
+  borderBottom: "none",
+};
+
 import { Link } from "react-router-dom";
 
 function AllTasksView({tasks}) {
   if (!tasks.length) {
     return (
-      <div>There are no tasks.</div>
+      <>
+        <Link to={`/`}><button>Back to Home</button></Link>
+        <div>There are no tasks.</div>
+      </>
     );
   }
   return (
     <>
-      <ul>
-        {tasks.map((todo, idx) => (
-          <li key={todo.id}>Task #{idx+1}: {todo.title}</li>
-        ))}
-      </ul>
       <Link to={`/`}><button>Back to Home</button></Link>
+      <ul style={ulStyle}>
+        {tasks.map((todo, idx) => {
+          let styleBool = idx === tasks.length - 1 ? liStyleLastChild : liStyle;
+          return <li key={todo.id} style={styleBool}>Task #{idx+1}: {todo.title}</li>;
+        })}
+      </ul>
     </>
   );
 
