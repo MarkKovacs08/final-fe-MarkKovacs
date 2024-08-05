d2e3e2c0e32a494717ec106aed1b3790958116e9
Melissa <lynch.melissat@gmail.com>
2024-08-05 12:14:34 -0400

    move markup to SingleTaskView

--- src/components/containers/SingleTaskContainer.jsx
+++ src/components/containers/SingleTaskContainer.jsx
@@ -1,6 +1,8 @@
 import { useParams } from 'react-router-dom';
 import { useSelector } from "react-redux";
 
+import SingleTaskView from '../views/SingleTaskView';
+
 function SingleTaskContainer() {
   let { taskId } = useParams(); //get id from URL
   taskId = parseInt(taskId); //convert to integer
@@ -10,22 +12,7 @@ function SingleTaskContainer() {
     state.tasks.find(task => task.id === taskId)
   );
 
-  if (!task) {
-    return (
-      <section>
-        <h2>Task not found!</h2>
-      </section>
-    );
-  }
-
-  return (
-    <section>
-      <article className="task">
-        <h2>{task.title}</h2>
-        <p className="task-title">{task.title}</p>
-      </article>
-    </section>
-  );
+  return <SingleTaskView task={task}/>
 }
 
 export default SingleTaskContainer;
