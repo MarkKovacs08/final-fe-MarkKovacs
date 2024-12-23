979aaa7f96cf514bd3c32d430aa268434f97d76c
Mark Kovacs <ItsMark287@andreas-air-7.mynetworksettings.com>
2024-12-23 13:31:15 -0500

    added add and delete emp

--- src/components/views/AllEmployeesView.jsx
+++ src/components/views/AllEmployeesView.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-function AllEmployeesView({employees}) {
+function AllEmployeesView({employees, deleteEmployee}) {
   if (!employees.length) {
     return (
       <div>There are no employees.</div>
@@ -8,9 +8,10 @@ function AllEmployeesView({employees}) {
   }
   return (
     <>
+      <Link to={`/employees/new`}><button style={{margin: "8px"}}>Add Employee</button></Link>
       <ul>
         {employees.map((user, idx) => (
-          <li key={user.id}>Employee #{idx+1}: {user.firstname}</li>
+          <li key={user.id}>Employee #{idx+1}: {user.firstname} <button onClick={() => deleteEmployee(user.id)}>Delete user</button></li>
         ))}
       </ul>
       <Link to={`/`}><button>Back to Home</button></Link>
--- src/store/employeesSlice.js
+++ src/store/employeesSlice.js
@@ -24,4 +24,23 @@ export const fetchEmployees = () => async (dispatch) => {
     console.error(err);
   }
 };
-
+//DELETE EMPLOYEE
+export const deleteEmployee = employeeId => async dispatch => {
+  try{
+    await axios.delete(`${PATH}/${employeeId}`);
+    //delete succesful so change state with dispatch
+    dispatch({type: 'employees/employeeDeleted', payload: employeeId});
+  } catch(err) {
+    console.error(err);
+  }
+}; 
+/* ADD EMPLOYEE */
+export const addEmployee = employee => async (dispatch) => {
+  try {
+    let res = await axios.post(`${PATH}`, employee);
+    dispatch({type: 'employees/employeeCreated', payload: res.data});
+    return res.data;
+  } catch(err) {
+    console.error(err);
+  }
+};
